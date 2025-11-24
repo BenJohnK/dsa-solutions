9bcffe20aa283fca8b2968438ef9dcae7cda0a2d
ben <benjohn.mec@gmail.com>
2025-11-24 22:34:39 +0530

    done

---
+++ Tree/binary_tree_simple_implementation.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self, x):
+        self.data = x
+        self.left = None
+        self.right = None
+
+#driver code
+
+root = Node(10)
+root.left = Node(20)
+root.right = Node(30)
+root.left.left = Node(40)
+
