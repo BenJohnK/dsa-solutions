60a1fb778e56e26d8f3b1cbb9f3d696a06dc4588
Ben John <benjohn.mec@gmail.com>
2026-03-31 14:39:58 +0530

    improved code of finding leftmost non repeating character in a string program

--- String/find_leftmost_non_repeating_character.py
+++ String/find_leftmost_non_repeating_character.py
@@ -3,11 +3,11 @@ import sys
 
 def find_leftmost_non_repeating_character(s: str):
     counter_dict = {}
-    for x in s.lower():
-        counter_dict[x] = counter_dict.get(x, 0) + 1
-    for k in counter_dict:
-        if counter_dict[k] == 1:
-            return k
+    for ch in s.lower():
+        counter_dict[ch] = counter_dict.get(ch, 0) + 1
+    for ch in s.lower():
+        if counter_dict[ch] == 1:
+            return ch
     return -1
 
-print(find_leftmost_non_repeating_character("teeter"))
+print(find_leftmost_non_repeating_character("ben"))
