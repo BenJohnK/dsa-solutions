864cbada2e8e780026f28701e5030f054f8ab94e
Ben John <benjohn.mec@gmail.com>
2026-04-04 20:37:47 +0530

    implemented preorder traversal and postorder traversal for binary tree

---
+++ Tree/post_order_traversal.py
@@ -0,0 +1,24 @@
+class ListNode:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+
+root = ListNode(10)
+root.left = ListNode(20)
+root.left.left = ListNode(40)
+root.left.right = ListNode(50)
+root.left.right.left = ListNode(70)
+root.left.right.right = ListNode(80)
+root.right = ListNode(30)
+root.right.right = ListNode(60)
+
+def post_order(root):
+    if not root:
+        return
+    post_order(root.left)
+    post_order(root.right)
+    print(root.key)
+
+post_order(root)
